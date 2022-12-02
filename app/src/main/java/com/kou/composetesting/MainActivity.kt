82382b15771288ad67765f1de5158ccd973ab044
shk-cod <konstantin0780@gmail.com>
2022-12-02 12:30:07 +0300

    lesson 1 finished

--- app/src/main/java/com/kou/composetesting/MainActivity.kt
+++ app/src/main/java/com/kou/composetesting/MainActivity.kt
@@ -1,15 +1,127 @@
 package com.kou.composetesting
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kou.composetesting.ui.theme.ComposeTestingTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            ComposeTestingTheme {
+                Conversation(SampleData.conversationSample)
+            }
         }
+    }
+}
+
+data class Message(
+    val author: String,
+    val body: String
+)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.kou_avatar),
+            contentDescription = "Kou avatar picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        Column(
+            modifier = Modifier.clickable { isExpanded = !isExpanded }
+        ) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            
+            Spacer(modifier = Modifier.height(4.dp))
+            
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
+    
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        messages.map { item { MessageCard(it) } }
+    }
+}
+
+/**
+ * for the correct color of the dark theme background,
+ * use DayNight theme in themes.xml
+ */
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun MessageCardPreview() {
+    ComposeTestingTheme {
+        MessageCard(Message(
+            "Android",
+            "Jetpack composable"
+        ))
+    }
+}
+
+@Preview
+@Composable
+fun ConversationPreview() {
+    ComposeTestingTheme {
+        Conversation(SampleData.conversationSample)
     }
 }
