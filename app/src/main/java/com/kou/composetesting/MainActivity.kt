e114e47d4cae92ba04b5b63ebf0a286b97d547f5
shk-cod <konstantin0780@gmail.com>
2022-12-04 18:47:20 +0300

    codelab 1 finished

--- app/src/main/java/com/kou/composetesting/MainActivity.kt
+++ app/src/main/java/com/kou/composetesting/MainActivity.kt
@@ -6,35 +6,73 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.repeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kou.composetesting.ui.theme.ComposeTestingTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTestingTheme {
-                Conversation(SampleData.conversationSample)
+                var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+                if (shouldShowOnboarding)
+                    OnboardingScreen (onContinueClicked = { shouldShowOnboarding = false } )
+                else
+                    Conversation(SampleData.conversationSample)
             }
         }
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to Compose testing app!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 320
+)
+@Composable
+fun OnboardingPreview() {
+    ComposeTestingTheme {
+        OnboardingScreen( onContinueClicked = {} )
     }
 }
 
@@ -59,7 +97,11 @@ fun MessageCard(msg: Message) {
 
         var isExpanded by remember { mutableStateOf(false) }
         val surfaceColor by animateColorAsState(
-            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+            animationSpec = repeatable(
+                iterations = 2,
+                animation = tween()
+            )
         )
 
         Column(
@@ -77,7 +119,9 @@ fun MessageCard(msg: Message) {
                 shape = MaterialTheme.shapes.medium,
                 elevation = 1.dp,
                 color = surfaceColor,
-                modifier = Modifier.animateContentSize().padding(1.dp)
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
             ) {
                 Text(
                     text = msg.body,
