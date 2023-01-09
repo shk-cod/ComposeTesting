34b241d3dfb3ac80a862ea5ebe19cb011aa4ee30
shk-cod <konstantin0780@gmail.com>
2023-01-09 20:58:44 +0300

    added "align your body" element

--- app/src/main/java/com/kou/composetesting/CodelabBasicLayouts.kt
+++ app/src/main/java/com/kou/composetesting/CodelabBasicLayouts.kt
@@ -1,15 +1,23 @@
 package com.kou.composetesting
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.kou.composetesting.ui.theme.ComposeTestingTheme
 
 @Composable
 fun SearchBar(
@@ -36,8 +44,50 @@ fun SearchBar(
     )
 }
 
+@Composable
+fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(drawable),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape)
+
+        )
+        Text(
+            text = stringResource(text),
+//            style = MaterialTheme.typography.h3,
+            modifier = Modifier.paddingFromBaseline(
+                top = 24.dp, bottom = 8.dp
+            )
+        )
+    }
+}
+
 @Preview
 @Composable
 fun SearchBarPreview() {
     SearchBar()
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun AlignYourBodyElementPreview() {
+    ComposeTestingTheme {
+        AlignYourBodyElement(
+            drawable = R.drawable.ab1_inversions,
+            text = R.string.ab1_inversions,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+
 }
