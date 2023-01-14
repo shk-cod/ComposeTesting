a09e3d813d009f44a336304bb500690457adb327
shk-cod <konstantin0780@gmail.com>
2023-01-14 19:53:22 +0300

    added "align your body row" element

--- app/src/main/java/com/kou/composetesting/CodelabBasicLayouts.kt
+++ app/src/main/java/com/kou/composetesting/CodelabBasicLayouts.kt
@@ -4,6 +4,8 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -18,6 +20,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kou.composetesting.ui.theme.ComposeTestingTheme
+
+const val previewBackgroundColor = 0xFFF0EAE2
 
 @Composable
 fun SearchBar(
@@ -102,16 +106,31 @@ fun FavoriteCollectionCard(
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun AlignYourBodyRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(alignYourBodyData) {item ->
+            AlignYourBodyElement(item.drawable, item.text)
+        }
+    }
+}
+
+
+@Preview(showBackground = true, backgroundColor = previewBackgroundColor)
 @Composable
 fun SearchBarPreview() {
     ComposeTestingTheme {
         SearchBar(modifier = Modifier.padding(8.dp))
     }
-
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Preview(showBackground = true, backgroundColor = previewBackgroundColor)
 @Composable
 fun AlignYourBodyElementPreview() {
     ComposeTestingTheme {
@@ -123,7 +142,7 @@ fun AlignYourBodyElementPreview() {
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Preview(showBackground = true, backgroundColor = previewBackgroundColor)
 @Composable
 fun FavoriteCollectionCardPreview() {
     ComposeTestingTheme {
@@ -134,3 +153,36 @@ fun FavoriteCollectionCardPreview() {
         )
     }
 }
+
+@Preview(showBackground = true, backgroundColor = previewBackgroundColor)
+@Composable
+fun AlignYourBodyRowPreview() {
+    ComposeTestingTheme {
+        AlignYourBodyRow(
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+private val alignYourBodyData = listOf(
+    R.drawable.ab1_inversions to R.string.ab1_inversions,
+    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
+    R.drawable.ab3_stretching to R.string.ab3_stretching,
+    R.drawable.ab4_tabata to R.string.ab4_tabata,
+    R.drawable.ab5_hiit to R.string.ab5_hiit,
+    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
+).map { DrawableStringPair(it.first, it.second) }
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableStringPair(it.first, it.second) }
+
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
